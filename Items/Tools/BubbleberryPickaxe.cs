c40609be3290beff53e38e62d50f54a2c64de565
ValkyrieValhalla <starwrathtcf@gmail.com>
2022-08-01 17:38:37 -0500

    Base Bubbleberry Pickaxe
    lone pickaxe vers of the bubbleberry pickaxe

---
+++ Items/Tools/BubbleberryPickaxe.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace ContentCrate.Items.Tools
+{
+    public class BubbleberryPickaxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bubbleberry Pickaxe");
+            Tooltip.SetDefault("Right-click while holding to unleash a bunch of bubbleberries!");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 12;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 44;
+            Item.height = 44;
+            Item.useTime = 13;
+            Item.useAnimation = 13;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 4;
+            Item.value = Item.sellPrice(0, 0, 50, 0);
+            Item.rare = ItemRarityID.Orange;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.useTurn = true;
+            Item.pick = 55;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe(1);
+            recipe.AddIngredient(ItemID.JungleSpores, 10);
+            recipe.AddIngredient(ItemID.Stinger, 8);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
+    }
+}
